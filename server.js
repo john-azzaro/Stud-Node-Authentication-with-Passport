6d6ee34a01da46c2977fd15687dcd59318899478
John Azzaro <jazzaro@temple.edu>
2019-09-28 14:03:30 -0700

    added views, html, form, post route for form

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();                             // get app variable from expr
 
 app.set('view-engine', 'ejs');
 
+
+
 app.get('/', function(req, res) {                  // Route for Home Page (that you need to be logged into to get to)
     res.render('index.ejs', { name: 'joe'});                       // The response will be to render a "index.ejs" page.
 });
@@ -10,9 +12,16 @@ app.get('/', function(req, res) {                  // Route for Home Page (that 
 app.get('/login', function (req, res) {                // Route for Login Page
     res.render('login.ejs');                           // ... which renders the login.ejs view.
 });
-app.get('/registration', function (req, res) {          // Route for Registration Page
-    res.render('registration');                         // ... which renders the registration.ejs view.
+
+app.get('/register', function (req, res) {          // Route for Registration Page
+    res.render('register.ejs');                         // ... which renders the registration.ejs view.
 });
+
+app.post('/register', function(req, res) {
+    
+});
+
+
 
 app.listen(process.env.PORT || 3000, function() {                                             // listen on port 3000 for requests...
     console.log(`Your app is listening on port ${process.env.PORT || 3000}...`);
