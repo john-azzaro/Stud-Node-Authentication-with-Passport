71c6be4cffd5167f5b2bbebdf1612774935202f6
John Azzaro <jazzaro@temple.edu>
2019-09-27 17:32:14 -0700

    home page route, views

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');                // import express
 const app = express();                             // get app variable from express
 
+app.get('/', function(req, res) {                  // Home page route (that you need to be logged into to get to)
+    res.render('index.ejs');                       // The response will be to render a "index.ejs" page.
+});
+
+
+
 app.listen(process.env.PORT || 3000, function() {
     console.log(`Your app is listening on port ${process.env.PORT || 3000}...`);
 });
