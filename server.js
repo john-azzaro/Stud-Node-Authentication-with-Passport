ace3573dcd644c7c752466ba420afd3c17cbf984
John Azzaro <jazzaro@temple.edu>
2019-09-28 13:35:34 -0700

    added login and registration views, etc

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const app = express();                             // get app variable from expr
 
 app.set('view-engine', 'ejs');
 
-app.get('/', function(req, res) {                  // Home page route (that you need to be logged into to get to)
+app.get('/', function(req, res) {                  // Route for Home Page (that you need to be logged into to get to)
     res.render('index.ejs', { name: 'joe'});                       // The response will be to render a "index.ejs" page.
 });
 
-
+app.get('/login', function (req, res) {                // Route for Login Page
+    res.render('login.ejs');                           // ... which renders the login.ejs view.
+});
+app.get('/registration', function (req, res) {          // Route for Registration Page
+    res.render('registration');                         // ... which renders the registration.ejs view.
+});
 
 app.listen(process.env.PORT || 3000, function() {                                             // listen on port 3000 for requests...
     console.log(`Your app is listening on port ${process.env.PORT || 3000}...`);
