1f38e7730aa1838d65a9bef18e4d44fdd2529304
John Azzaro <jazzaro@temple.edu>
2019-09-28 23:13:40 -0700

    passport strategy

--- passport-config.js
+++ passport-config.js
@@ -1,5 +1,7 @@
-function initialize(passport) {
+const LocalStrategy = require('passport-local').Strategy           // to use the local version of passport, load passport-local as a strategy.
 
+function initialize(passport) {
+    passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))
 }
 
 module.exports = initialize;
