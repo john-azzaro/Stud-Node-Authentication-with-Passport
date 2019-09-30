cae171f0771189e876507a18fa489e022a58f39e
John Azzaro <jazzaro@temple.edu>
2019-09-29 20:33:30 -0700

    authenticate user issue resolved

--- passport-config.js
+++ passport-config.js
@@ -1,7 +1,13 @@
 const LocalStrategy = require('passport-local').Strategy           // to use the local version of passport, load passport-local as a strategy.
 
+
+
+
 function initialize(passport) {
-    passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))
+    const authenticateUser = function(email, password, done) {                  // Second, have authenticateUser with email and password passed in and then done when we are done authenticting the user. 
+ 
+    };
+    passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))   // First, use local strategy and pass user name field (i.e. email but it could be name or whatever) and the call to authenticate the user.
 }
 
 module.exports = initialize;
