8afa3c17e43cfb273f449f0ca8c3a428a04d53a1
John Azzaro <jazzaro@temple.edu>
2019-09-29 21:21:41 -0700

    added flash and session

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express = require('express');                // import express
-const app = express();                             // get app variable from express
-const bcrypt = require('bcrypt');                  // added bcrypt for security
-const passport = require('passport');              // add the passport library
+const express = require('express');                         // import express
+const app = express();                                      // get app variable from express
+const bcrypt = require('bcrypt');                           // added bcrypt for security
+const passport = require('passport');                       // add the passport library
+const flash = require("express-flash");                     // add flash (for messages)
+const session = require("express-session");                 // add session to persist login for user across app.
 
 const initializePassport = require('./passport-config');     // call the passport config file for passport information
-initializePassport(passport, email => users.find(user => user.email === email));       // Function for finding user based on the email and the passport we are configuring
+initializePassport(passport, email => {
+    return users.find(user => user.email === email )});       // Function for finding user based on the email and the passport we are configuring
                                
 
 
@@ -12,7 +15,7 @@ const users = [];                                          // local storage for 
 
 app.set('view-engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
-
+app.use(flash())
 
 
 
@@ -20,17 +23,12 @@ app.get('/', function(req, res) {                          // Route for Home Pag
     res.render('index.ejs', { name: 'joe'});               // The response will be to render a "index.ejs" page.
 });
 
-
-
-
 app.get('/login', function (req, res) {                    // Route for Login Page
     res.render('login.ejs');                               // ... which renders the login.ejs view.
 });
 app.post('/login', function(req, res) {                    // Route to POST login
     
 });
-
-
 
 app.get('/register', function (req, res) {                 // Route for Registration Page
     res.render('register.ejs');                            // ... which renders the registration.ejs view.
@@ -50,7 +48,6 @@ app.post('/register', async function(req, res) {                               /
         res.redirect('/register', );                                           // and respond by redirecting to the register page.
     }                                                    
 });
-
 
 app.listen(process.env.PORT || 3000, function() {                                             // listen on port 3000 for requests...
     console.log(`Your app is listening on port ${process.env.PORT || 3000}...`);
