86c62eddd46bf0924426ea1b0327989215fd1f9e
John Azzaro <jazzaro@temple.edu>
2019-10-01 09:12:34 -0700

    post login authentication issue

--- server.js
+++ server.js
@@ -12,21 +12,19 @@ const session = require("express-session");                 // add session to pe
 
 const initializePassport = require('./passport-config');     // call the passport config file for passport information
 initializePassport(passport, email => {
-    return users.find(user => user.email === email )});       // Function for finding user based on the email and the passport we are configuring
-                             
+    return users.find(user => user.email === email )});       // Function for finding user based on the email and the passport we are configuring                           
 
 const users = [];                                          // local storage for users (would be mongodb in production)
 
 app.set('view-engine', 'ejs');                             // use ejs view engine
-app.use(express.urlencoded({ extended: false }));          // tells the application to take the email and password input and use them in the methods.
 
+app.use(express.urlencoded({ extended: false }));          // tells the application to take the email and password input and use them in the methods.
 app.use(flash());                                          // use the flash middleware
 app.use(session( {                                         // use the session middleware
     secret: process.env.SESSION_SECRET,                    // the "secret" is a key that will encrypt all the information which is gotten from environment variables, of which "SESSION_SECRET" will be put inside .env file.
     resave: false,                                         // should we resave the session variables if nothing has changed (we dont so its false)
     saveUninitialized: false,                              // should you save an empty value in the session... we dont so its false.
 }));
-
 app.use(passport.initialize());                            // Setup passport middleware to setup basics.
 app.use(passport.session());                                // to store variables across the entire session, use passport.session middleware (works with app.use(session) above).
 
@@ -38,9 +36,11 @@ app.get('/', function(req, res) {                          // Route for Home Pag
 app.get('/login', function (req, res) {                    // Route for Login Page
     res.render('login.ejs');                               // ... which renders the login.ejs view.
 });
-app.post('/login', function(req, res) {                    // Route to POST login
-    
-});
+                                                            // To use passport as our authentication for login, we use passport.authenticate with the "local" strategy and pass a list of option of things to modify (i.e. where to go if there is a success, failure , etc).
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',                                    // ... if the login is successful, redirect to home page.
+    failureRedirect: '/login'                                // ... if the login is unsuccessful, redirect back to login screen.
+}));       
 
 app.get('/register', function (req, res) {                 // Route for Registration Page
     res.render('register.ejs');                            // ... which renders the registration.ejs view.
