d41cafeec667c5ba050c4df1f02aa0886685776d
John Azzaro <jazzaro@temple.edu>
2019-10-01 09:29:30 -0700

    passport issue

--- passport-config.js
+++ passport-config.js
@@ -3,8 +3,8 @@ const bcrypt = require('bcrypt')                                  // take in bcr
 
 
 
-function initialize(passport, getUserByEmail, getUserById) {
-    const authenticateUser = async function(email, password, done) {                       // Second, have authenticateUser with email and password passed in and then done when we are done authenticting the user. This is what is called from our email password to make sure the user is correct.
+async function initialize(passport, getUserByEmail, getUserById) {
+    const authenticateUser = async function(email, password, done) {              // Second, have authenticateUser with email and password passed in and then done when we are done authenticting the user. This is what is called from our email password to make sure the user is correct.
     const user =  getUserByEmail(email)                                           // 1. Get user by email or return null if there is no email (at this point the function has not been created yet but planning ahead)
        
     if(user === null) {                                                           // 2. Check to see if there is a user.
@@ -22,12 +22,9 @@ function initialize(passport, getUserByEmail, getUserById) {
     }                                    
 }; 
 
-    passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))   // First, use local strategy and pass user name field (i.e. email but it could be name or whatever) and the call to authenticate the user.
-    passport.serializeUser(function(user, done) {                                    // Third, to serialize the user and store inside the session, call serializeUser take a function and pass user and done to store inside the session.
-    passport.deserializeUser(function(id, done) {                                    // Fourth, deserialize outside session if needed.
-
-    })
-    });
+    passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser));   // First, use local strategy and pass user name field (i.e. email but it could be name or whatever) and the call to authenticate the user.
+    passport.serializeUser(function(user, done) {});                                    // Third, to serialize the user and store inside the session, call serializeUser take a function and pass user and done to store inside the session.
+    passport.deserializeUser(function(id, done) {});                                   // Fourth, deserialize outside session if needed.
 }
 
 module.exports = initialize;
